d53c3d5ae221b3e70fc079cc3bcf12cb02143854
SaiTheinThanTun <theinthantun.sai@gmail.com>
2016-09-04 09:36:04 +0630

    working Rcpp version

--- R/stRCPP.cpp
+++ R/stRCPP.cpp
@@ -10,6 +10,10 @@ NumericMatrix stateT(int origin, IntegerVector newstates, NumericVector cumuprob
   int ncases = sMatrix.nrow();
   double last_probs = cumuprobs[0];
 
+  //  adjustment for the state index (R starts at 1 and C++ starts at 0)
+  newstates = newstates-1;
+  origin = origin-1;
+
   // for loop for each new state starts here
   // remember that the index in C++ starts at 0
   for (int i=0; i<newstates.size(); i++){
@@ -26,14 +30,14 @@ NumericMatrix stateT(int origin, IntegerVector newstates, NumericVector cumuprob
     LogicalVector tmp = rno < probs_for;
     NumericVector test1 = as<NumericVector> (tmp);
     // test which cases will advance to next state based on the cumulative probabilities (cumuprobs)
-    tmp = rno > last_probs;
-    NumericVector test2 = as<NumericVector> (tmp);
+    LogicalVector tmp2 = rno > last_probs;
+    NumericVector test2 = as<NumericVector> (tmp2);
 
-    double change = sMatrix(_,origin) * test1 * test2;
+    NumericVector change = sMatrix(_,origin) * test1 * test2;
 
     // subset the sMatrix for origin and newstate(s)
     sMatrix(_,newstateScalar) = sMatrix(_,newstateScalar) + change; //equation here: s.matrix[,i]+(s.matrix[,origin]*(rand<probs_for)*(rand>last_prob)
-    sMatrix(_,origin) = sMatrix(_,origin) + change; //equation here: s.matrix[,origin]-(s.matrix[,origin]*(rand<probs_for)*(rand>last_prob))
+    sMatrix(_,origin) = sMatrix(_,origin) - change; //equation here: s.matrix[,origin]-(s.matrix[,origin]*(rand<probs_for)*(rand>last_prob))
 
     // advance the last_probs
     last_probs = probs_for;
@@ -46,7 +50,7 @@ NumericMatrix stateT(int origin, IntegerVector newstates, NumericVector cumuprob
 
 
 /*** R
-#p <- syn_pop(c(2,3,1))
-stateT(2, 3)
-stateT(c(3,2), c(2,9))
+p <- syn_pop(c(2,3,1))
+stateT(2, 3, c(.5,1), p)
+#stateT(c(3,2), c(2,9))
 */
