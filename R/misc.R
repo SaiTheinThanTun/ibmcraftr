c55d6ecf59cd4058f3399be9f44ce4a7c86e9ed1
SaiTheinThanTun <theinthantun.sai@gmail.com>
2016-11-16 11:03:12 +0700

    misc.R to hold miscellaneous functions

---
+++ R/misc.R
@@ -0,0 +1,15 @@
+#' Miscellaneous functions to support the \code{ibmcraftr} packare are here.
+#'
+#' @param rates A numeric scalar or vector to be transformed into rates.
+#' @return A numeric scalar or vector in terms of probabilities.
+#' @examples
+#' rate2prob(c(.1, .5))
+#'
+#' @export
+#'
+
+#misc functions
+
+rate2prob <- function(rates){
+  1-exp(-rates*1)
+}
